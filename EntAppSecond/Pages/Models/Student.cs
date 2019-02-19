da287fce3facb18e36fc2a87a3e5acf0eb6ac46a
HPLaptop <HPLaptop@CLEMENTLAPTOP>
2019-02-19 17:24:05 +0000

    update

---
+++ EntAppSecond/Pages/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntAppSecond.Pages.Models
+{
+    public class Student
+    {
+
+        
+
+        [Required(ErrorMessage = "Please enter a student id")]
+        public string StudentID { get; set; } = "";
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = "";
+
+
+        
+
+        [Required]
+        public string LastName { get; set; } = "";
+
+    }
+}
---
+++ EntAppSecond/Pages/Students/Create.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using EntAppSecond.Pages.Models;
+
+namespace EntAppSecond.Pages
+{
+    public class CreateModel : PageModel
+    {
+
+        public string Message { get; set; }
+
+        [BindProperty]
+        public Student Student { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+            Message = "post has just happened";
+
+            if (ModelState.IsValid)
+            {
+                Message += "and all is well with the world";
+            }
+            else
+            {
+                Message+="Heuston WE HAVE A PROBLEM";
+            }
+        }
+    }
+}
