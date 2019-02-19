a5b2a93339a12c733265b576fee0c7c40e984739
HPLaptop <HPLaptop@CLEMENTLAPTOP>
2019-02-19 14:20:49 +0000

    update

---
+++ EntAppSecond/Pages/FormWS.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntAppSecond.Pages
+{
+    public class FormWSModel : PageModel
+    {
+        [BindProperty]
+        public string ProductCode { get; set; }
+        [BindProperty]
+        public string Colour { get; set; }
+        [BindProperty]
+        public string Consent { get; set; }
+
+        public String Message { get; set; } = "";
+        public String Message1 { get; set; } = "";
+        public String Message2 { get; set; } = "";
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+            Message = string.Format("The coloue is {0}", Colour);
+            Message1 = string.Format("The product id is {0}", ProductCode);
+            Message2= string.Format("oK i CONSENT {0}",Consent);
+
+
+        }
+    }
+}
