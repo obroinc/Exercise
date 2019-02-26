6d2c7219c393e91c5589339ef678aef02d54d8c0
HPLaptop <HPLaptop@CLEMENTLAPTOP>
2019-02-26 18:41:26 +0000

    hyt

--- EntAppSecond/Pages/Models/Student.cs
+++ EntAppSecond/Pages/Models/Student.cs
@@ -10,19 +10,22 @@ namespace EntAppSecond.Pages.Models
     {
        //https//docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/ validation?view=aspnetcore-2.2*/
 
-        [RegularExpression(@"(S|s)\d{7}")]
+        [RegularExpression(@"(S|s)\d{7}")]//Student number desins with S/s followed by 7 no.
+        [Display(Name="Please supply student ID")]//Message diaplayed for user-lable
         [Required ]
         public string StudentID { get; set; } = "";
 
-        [RegularExpression(@"\w\'-,\s]{2,}")]
-        [Required(ErrorMessage = "Please enter your First Name")]
+        [RegularExpression(@"\w\'-,\s]{2,}", ErrorMessage="Must be at leat 2 chatacters long")]
+        //@\w(2,50) will also work fine-letters stc with a min of 2 char and max of 50
+        //The error massage will be displayed instead of red expression
+        [Required(ErrorMessage = "Please enter your First Name")]//error message displayed
         [Display(Name = "First Name")]
-        [StringLength(60, MinimumLength = 2)]
+        [StringLength(60, MinimumLength = 2)]//min of 2 characters
         public string FirstName { get; set; } = "";
 
 
         [Display(Name = "Last Name")]
-        [RegularExpression(@"\w\'-,\s]{2,}")]
+        [RegularExpression(@"\w\'-,\s]{2,}")]//also @.(3,) gives  min of 3 char
         [Required(ErrorMessage = "Please enter your Last Name")]
         [StringLength(60, MinimumLength = 3)]//least number of 3 characters in the string
         public string LastName { get; set; } = "";
@@ -35,12 +38,14 @@ namespace EntAppSecond.Pages.Models
 
 
         //date put in a text box
-       
+        [Required(ErrorMessage = "Please enter DOB as dd-mm-yyyy")]
+        [RegularExpression(@"^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$")]
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DateOfBirth { get; set; }
 
 
         [Required]
+        //[Range(1,int32.MaxValue)]
         [Display(Name = "Number of Modules")]
         public int NumberOfModules { get; set; }
 
