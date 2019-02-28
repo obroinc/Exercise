3700de8240b27857f31c98b167b80ddc9c10cf3f
HPLaptop <HPLaptop@CLEMENTLAPTOP>
2019-02-28 21:18:03 +0000

    update

--- Databases/Pages/ListStudents.cshtml.cs
+++ Databases/Pages/ListStudents.cshtml.cs
@@ -5,26 +5,27 @@ using System.Threading.Tasks;
 using Databases.Pages.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore; 
 
 namespace Databases.Pages
 {
     public class ListStudentsModel : PageModel
     {
-       /* private readonly StudentContext _db;
+        private readonly StudentContext _db;
 
-       // public CreateStudentModel(StudentContext db)
-       // {
-       //     _db = db;
+       public ListStudentsModel(StudentContext db)
+       {
+            _db = db;
         }
 
 
-       // public IList<Student> StudentsDB { get; private set; }
-       // public async Task OnGetTaskAsync()
+       public IList<Student> StudentsDB { get; private set; }
+       public async Task OnGetTaskAsync()
 
         
-      //  {
+       {
 
-            StudentsDB = await _db.Students.AsNoTrackingg().ToListAsync();
+            //StudentsDB = await _db.Students.AsNoTracking().ToListAsync();
         }
-    }*/
+    }
 }
